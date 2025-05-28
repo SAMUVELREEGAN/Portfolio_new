6b7c729c7482bc217d8f7659e0accc1ed0099276
Samuvel Reegan <samuelreegan372@gmail.com>
2025-05-28 13:39:26 +0530

    Allow portfolio-new-cf1e.onrender.com in vite allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'], 
-server: {
-  host: true,
-  allowedHosts: [
-    '.ngrok-free.app', // wildcard allow ngrok domains
-    'localhost',
-    '127.0.0.1',
-  ],
-}
-
+  server: {
+    host: true,
+    allowedHosts: [
+      '.ngrok-free.app', 
+      'localhost',
+      '127.0.0.1',
+      'portfolio-new-cf1e.onrender.com',  // <--- add this line
+    ],
+  },
 });

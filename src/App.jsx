de1c414b27d02f754ce2cb987dbac8deba39f091
Samuvel Reegan <samuelreegan372@gmail.com>
2025-05-29 17:17:46 +0530

    Change my local dic check

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './Pages/Home'
 import MyLayout from './Layouts/MyLayout'
 import FullModelPage from './Components/FullModelPage'
 import Resume from './Components/Resume'
-// import WhatsAppIcon from './Components/WhatsAppIcon'
 
 function App() {
 
@@ -19,7 +18,6 @@ function App() {
     </Route>
     </Routes>
     </BrowserRouter>
-            {/* <WhatsAppIcon /> */}
     </>
   )
 }
